efb5aae31a233ae20e3715d9039bd7a734059340
Максим <Mt.Pm@yandex.ru>
2022-11-30 13:33:41 +0700

    На отправку

---
+++ 64/Program.cs
@@ -0,0 +1,23 @@
+﻿//  Задайте значение N.
+//   Напишите программу, которая выведет все натуральные числа в промежутке от N до 1. 
+//   Выполнить с помощью рекурсии.
+
+
+string NumbersRec(int M, int N)
+{
+    if (M <= N) 
+    {
+        return NumbersRec(M, N -1) + $"{N} ";
+    }
+    else return String.Empty;
+}
+System.Console.Write("Write M: ");
+int M = Convert.ToInt32(System.Console.ReadLine());
+
+System.Console.WriteLine("Write N: ");
+int N = Convert.ToInt32(System.Console.ReadLine());
+
+System.Console.WriteLine(NumbersRec(M, N));
+
+System.Console.WriteLine("Для продолжения нажмите любую кнопку:");
+System.Console.ReadKey();
---
+++ 66/Program.cs
@@ -0,0 +1,33 @@
+﻿// Задайте значения M и N.
+//  Напишите программу, которая найдёт сумму натуральных элементов в промежутке от M до N.
+
+System.Console.Write("Введите число M: ");
+int m = Convert.ToInt32(System.Console.ReadLine());
+
+System.Console.Write("Введите число N: ");
+int n = Convert.ToInt32(System.Console.ReadLine());
+
+SumFromMToN(m, n);
+
+void SumFromMToN(int m, int n)
+{
+    Console.Write(SumMN(m - 1, n));
+}
+
+int SumMN(int m, int n)
+{
+    int res = m;
+    if (m == n)
+    {
+        return 0;
+    }
+    else
+    {
+        m++;
+        res = m + SumMN(m, n);
+        return res;
+    }
+}
+System.Console.WriteLine();
+System.Console.WriteLine("Для продолжения нажмите любую кнопку:");
+System.Console.ReadKey();
